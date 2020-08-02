9c0a9c9b37a70b238c318154360bda239f09ab4a
Marcos JRS <marcosjrs80@gmail.com>
2020-08-02 13:13:18 +0200

    Cambios minimos, eliminacion warnings

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Link, BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import HomeContainer from './containers/HomeContainer';
 import CustomersContainer from './containers/CustomersContainer';
 import CustomerContainer from './containers/CustomerContainer';
--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
 import AppFrame from '../components/AppFrame'
 import CustomersAction from '../components/CustomersAction'
 import { withRouter } from 'react-router-dom'
